81d14f731802660faabf8ee27a9b1b687689c8c7
Igor Machado <igor.machado@gmail.com>
2023-09-12 14:01:26 +0000

    feat: using gmpxx on tests

--- tests/depbox_gmp.test.cpp
+++ tests/depbox_gmp.test.cpp
@@ -1,11 +1,22 @@
 // SPDX-License-Identifier:  MIT
 // Copyright (C) 2023
 
+#include <iostream>
+//
 #include <catch2/catch_all.hpp>
 //
-#include <gmp.h>  // NOLINT
+#include <gmp.h>    // NOLINT
+#include <gmpxx.h>  // NOLINT
 
 TEST_CASE("Teste1-X") {
   int x = 10;
   REQUIRE(x == 10);
+}
+
+TEST_CASE("Teste2-X") {
+  mpz_class a, b, c;
+  a = 1234;
+  b = "-5678";
+  c = a + b;
+  REQUIRE(c == -4444);
 }
