b406a313db09bc7f5e42229cdc936facb4684d6e
Igor Machado <igor.machado@gmail.com>
2023-09-12 13:47:58 +0000

    feat: add catch2

--- script-deps.bat
+++ script-deps.bat
@@ -1,9 +1,9 @@
 @echo off
 
-../deps/vcpkg/vcpkg search gmp
+deps/vcpkg/vcpkg search gmp
 
-../deps/vcpkg/vcpkg search mpir
+deps/vcpkg/vcpkg search mpir
 
-../deps/vcpkg/vcpkg integrate install
+deps/vcpkg/vcpkg integrate install
 
-../deps/vcpkg/vcpkg install
+deps/vcpkg/vcpkg install
---
+++ tests/depbox_gmp.test.cpp
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier:  MIT
+// Copyright (C) 2023
+
+#include <catch2/catch_all.hpp>
+//
+#include <gmp.h>  // NOLINT
+
+TEST_CASE("Teste1-X") {
+  int x = 10;
+  REQUIRE(x == 10);
+}
