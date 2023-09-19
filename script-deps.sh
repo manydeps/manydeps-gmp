16bd10e89431c193591334b5e65571bb55d87758
Igor Machado Coelho <igor.machado@gmail.com>
2023-09-19 21:16:34 +0000

    Update script-deps.sh

--- script-deps.sh
+++ script-deps.sh
@@ -8,12 +8,14 @@ conan profile detect
 VCPKG_DIR=./tools/vcpkg
 VCPKG_TRIPLET=x64-windows-static
 VCPKG_BOOTSTRAP=bootstrap-vcpkg.sh
+USE_PRESET=win-x64-release
 # VCPKG_BOOTSTRAP=bootstrap-vcpkg.bat # NEVER RUN .bat here on .sh environment!
 
 COUNT_LINUX=`conan profile show | grep Linux | wc -l`
 if [[ "$COUNT_LINUX" -ne 0 ]]; then
     VCPKG_TRIPLET=x64-linux
     # VCPKG_BOOTSTRAP=bootstrap-vcpkg.sh
+    USE_PRESET=linux-release
 fi
 
 echo "VCPKG_TRIPLET=$VCPKG_TRIPLET"
@@ -34,7 +36,7 @@ $VCPKG_DIR/vcpkg --vcpkg-root $VCPKG_DIR integrate install
 
 $VCPKG_DIR/vcpkg --vcpkg-root $VCPKG_DIR install --triplet=$VCPKG_TRIPLET
 
-cmake -B build -S .
+cmake -B build -S . --preset=$USE_PRESET
 
 cmake --build build --config Release
 
